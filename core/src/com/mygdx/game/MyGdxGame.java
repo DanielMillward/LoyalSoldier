22301f585f9d0841d2d40a611b67b88a5ed5805c
DanielMillward <32293745+DanielMillward@users.noreply.github.com>
2017-12-11 12:35:53 +0200

    image assets + joystick alpha code
    Added images for player + joystick and added test joystick code. Untested

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,18 +2,69 @@ package com.mygdx.game;
 // testing
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
+import com.badlogic.gdx.scenes.scene2d.ui.Touchpad.*;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private Texture img;
+	private Camera camera;
+	private Touchpad touchpad;
+	private TouchpadStyle touchpadStyle;
+	private Skin touchpadSkin;
+	private Drawable touchBackground;
+	private Drawable touchKnob;
+	private Texture blockTexture;
+	private Sprite blockSprite;
+	private float blockSpeed;
+	private Stage stage;
+	private Viewport viewport;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		camera = new PerspectiveCamera();
+		viewport = new FitViewport(800, 480, camera);
+
+		touchpadSkin = new Skin();
+		//Set background image
+		touchpadSkin.add("touchBackground", new Texture("data/touchBackground.png"));
+		//Set knob image
+		touchpadSkin.add("touchKnob", new Texture("data/touchKnob.png"));
+		//Create TouchPad Style
+		touchpadStyle = new TouchpadStyle();
+		//Create Drawable's from TouchPad skin
+		touchBackground = touchpadSkin.getDrawable("touchBackground");
+		touchKnob = touchpadSkin.getDrawable("touchKnob");
+		//Apply the Drawables to the TouchPad Style
+		touchpadStyle.background = touchBackground;
+		touchpadStyle.knob = touchKnob;
+		//Create new TouchPad with the created style
+		touchpad = new Touchpad(10, touchpadStyle);
+		//setBounds(x,y,width,height)
+		touchpad.setBounds(15, 15, 200, 200);
+
+
+
+		//Create a Stage and add TouchPad
+		stage = new Stage(viewport, batch);
+		stage.addActor(touchpad);
+		Gdx.input.setInputProcessor(stage);
+
 	}
 
 	@Override
@@ -23,6 +74,8 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
 	}
 	
 	@Override
