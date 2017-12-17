f963c2d0840d3f6e19a53b2019198d49b2a031fe
DanielMillward <32293745+DanielMillward@users.noreply.github.com>
2017-12-17 12:30:57 +0200

    Made Actor class + copied code for box2d
    self explanatory

---
+++ core/src/com/mygdx/game/MyActor.java
@@ -0,0 +1,19 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+/**
+ * Created by David on 12/17/2017.
+ */
+
+public class MyActor extends Actor {
+    public MyActor() {
+
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+    }
+}
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,85 +2,107 @@ package com.mygdx.game;
 // testing
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
-import com.badlogic.gdx.scenes.scene2d.ui.Touchpad.*;
-import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
-import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 public class MyGdxGame extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Texture img;
-	private Camera camera;
-	private Touchpad touchpad;
-	private TouchpadStyle touchpadStyle;
-	private Skin touchpadSkin;
-	private Drawable touchBackground;
-	private Drawable touchKnob;
-	private Texture blockTexture;
-	private Sprite blockSprite;
-	private float blockSpeed;
-	private Stage stage;
-	private Viewport viewport;
+	SpriteBatch batch;
+	Sprite sprite;
+	Texture img;
+	World world;
+	Body body;
+	Stage stage;
 
-	
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		camera = new PerspectiveCamera();
-		viewport = new FitViewport(800, 480, camera);
+	public void create() {
+		stage = new Stage(new StretchViewport(640, 480));
 
-		touchpadSkin = new Skin();
-		//Set background image
-		touchpadSkin.add("touchBackground", new Texture("data/touchBackground.png"));
-		//Set knob image
-		touchpadSkin.add("touchKnob", new Texture("data/touchKnob.png"));
-		//Create TouchPad Style
-		touchpadStyle = new TouchpadStyle();
-		//Create Drawable's from TouchPad skin
-		touchBackground = touchpadSkin.getDrawable("touchBackground");
-		touchKnob = touchpadSkin.getDrawable("touchKnob");
-		//Apply the Drawables to the TouchPad Style
-		touchpadStyle.background = touchBackground;
-		touchpadStyle.knob = touchKnob;
-		//Create new TouchPad with the created style
-		touchpad = new Touchpad(10, touchpadStyle);
-		//setBounds(x,y,width,height)
-		touchpad.setBounds(15, 15, 200, 200);
-
-
-
-		//Create a Stage and add TouchPad
-		stage = new Stage(viewport, batch);
-		stage.addActor(touchpad);
 		Gdx.input.setInputProcessor(stage);
+		batch = new SpriteBatch();
+		// We will use the default LibGdx logo for this example, but we need a
+		//sprite since it's going to move
+		img = new Texture("badlogic.jpg");
+		sprite = new Sprite(img);
 
+		// Center the sprite in the top/middle of the screen
+		sprite.setPosition(Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2,
+				Gdx.graphics.getHeight() / 2);
+
+		// Create a physics world, the heart of the simulation.  The Vector
+		//passed in is gravity
+		world = new World(new Vector2(0, -98f), true);
+
+		// Now create a BodyDefinition.  This defines the physics objects type
+		//and position in the simulation
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.type = BodyDef.BodyType.DynamicBody;
+		// We are going to use 1 to 1 dimensions.  Meaning 1 in physics engine
+		//is 1 pixel
+		// Set our body to the same position as our sprite
+		bodyDef.position.set(sprite.getX(), sprite.getY());
+
+		// Create a body in the world using our definition
+		body = world.createBody(bodyDef);
+
+		// Now define the dimensions of the physics shape
+		PolygonShape shape = new PolygonShape();
+		// We are a box, so this makes sense, no?
+		// Basically set the physics polygon to a box with the same dimensions
+		//as our sprite
+		shape.setAsBox(sprite.getWidth()/2, sprite.getHeight()/2);
+
+		// FixtureDef is a confusing expression for physical properties
+		// Basically this is where you, in addition to defining the shape of the
+		//body
+		// you also define it's properties like density, restitution and others
+		//we will see shortly
+		// If you are wondering, density and area are used to calculate over all
+		//mass
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = shape;
+		fixtureDef.density = 1f;
+
+		Fixture fixture = body.createFixture(fixtureDef);
+
+		// Shape is the only disposable of the lot, so get rid of it
+		shape.dispose();
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void render() {
+
+		// Advance the world, by the amount of time that has elapsed since the
+		//last frame
+		// Generally in a real game, dont do this in the render loop, as you are
+		//tying the physics
+		// update rate to the frame rate, and vice versa
+		world.step(Gdx.graphics.getDeltaTime(), 6, 2);
+
+		// Now update the sprite position accordingly to it's now updated
+		//Physics body
+		sprite.setPosition(body.getPosition().x, body.getPosition().y);
+
+		// You know the rest...
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(sprite, sprite.getX(), sprite.getY());
 		batch.end();
-		stage.act(Gdx.graphics.getDeltaTime());
-		stage.draw();
+
+
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
+	public void dispose() {
+		// Hey, I actually did some clean up in a code sample!
 		img.dispose();
+		world.dispose();
 	}
 }
