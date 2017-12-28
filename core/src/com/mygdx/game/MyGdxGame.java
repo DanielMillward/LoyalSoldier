61a3a4bfe021d66f70201dc6a10152913430f03f
DanielMillward <32293745+DanielMillward@users.noreply.github.com>
2017-12-28 10:03:47 +0200

    broken
    probs as far as i'll go for now. To fix it i'd probably have to watch a whole tutorial set of videos, which kinda feels like cheating. I've done a lot of stuff and learned a lot, just not enough apparently.

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -75,17 +75,18 @@ public class MyGdxGame extends ApplicationAdapter {
 	int MAX_VELOCITY = 2;
 	static int direction = 1;
 	Body body;
-	int upVelocity = 0;
+	static int upVelocity = 0;
 	public static final int gravity = -10;
-	int upSpeed = upVelocity + gravity;
-	float moving;
+	static int upSpeed = upVelocity + gravity;
+	static float moving;
 	theGround ground;
 	static MyActor actor;
-	ArrayList<theGround> wallSpriteList = new ArrayList<theGround>();
-	ArrayList<Rectangle> wallList = new ArrayList<Rectangle>();
-	ArrayList<bullet> bulletList = new ArrayList<bullet>();
+	public static ArrayList<theGround> wallSpriteList = new ArrayList<theGround>();
+	public static ArrayList<Rectangle> wallList = new ArrayList<Rectangle>();
+	public static ArrayList<bullet> bulletList = new ArrayList<bullet>();
 
-
+int checkerInt;
+long otherCheckerInt;
 
 	@Override
 
@@ -250,38 +251,21 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		bullet newBullet;
 
-
-
+		if (otherCheckerInt == 0){
+			actor.setPosition(9, 9);
+	}
+	otherCheckerInt++;
 		if (upButton.isPressed()) {
 			jump();
 		}
 		if (leftButton.isPressed()) {
-			moving = 0.125f * -1 * PLAYER_SPEED;
+			moving = 0.02f * -1 * PLAYER_SPEED;
 			direction = -1;
 		}
 		if (rightButton.isPressed()) {
-			moving = 0.125f * PLAYER_SPEED;
+			moving = 0.02f * PLAYER_SPEED;
 			direction = 1;
 		}
-
-		for (int i = 0; i > 8; i++) {
-			int check = 0;
-			Rectangle ghost = new Rectangle(actor.getX() + moving, actor.getY() + upSpeed, PLAYER_WIDTH, PLAYER_HEIGHT);
-			for (Rectangle wall : wallList) {
-				if (!ghost.overlaps(wall)) {
-					check = check + 1;
-				}
-			}
-			if (check == wallList.size()) {
-				actor.setX(actor.getX() + moving);
-				actor.setY(actor.getY() + upSpeed);
-			}
-			check = 0;
-			moving = 0;
-			ghost = null;
-		}
-
-
 
 		stage.act(Gdx.graphics.getDeltaTime());
 
@@ -298,22 +282,44 @@ public class MyGdxGame extends ApplicationAdapter {
 			myTimer = myTimer + Gdx.graphics.getDeltaTime();
 		}
 
-		for (bullet bullet : bulletList) {
+		if (checkerInt >= 60) {
+			int check = 0;
+			Rectangle ghost = new Rectangle(actor.getX() + MyGdxGame.moving, actor.getY() + upSpeed, PLAYER_WIDTH, PLAYER_HEIGHT);
 			for (Rectangle wall : wallList) {
-				if (wall.contains(bullet.getX() + bullet.bulletDirection, bullet.getY())) {
-					bullet.remove();
-				} else {
-					bullet.setX(bullet.getX() + bullet.bulletDirection);
+				if (!ghost.overlaps(wall)) {
+					check = check + 1;
 				}
 			}
-		}
+			if (check == wallList.size()) {
+				actor.setX(actor.getX() + MyGdxGame.moving);
+				actor.setY(actor.getY() + upSpeed);
+			}
+			check = 0;
+			moving = 0;
+			ghost = null;
 
+			for (bullet bullet : bulletList) {
+				for (Rectangle wall : wallList) {
+					if (wall.contains(bullet.getX() + bullet.bulletDirection, bullet.getY())) {
+						bullet.remove();
+					} else {
+						bullet.setX(bullet.getX() + bullet.bulletDirection);
+					}
+				}
+			}
+			checkerInt = 0;
+		}
 		batch.begin();
 		stage.draw();
 		UIstage.draw();
 		batch.end();
-
+		if (myTimer > 1) {
+			System.out.println("DFM" + actor.getX() + " , " + actor.getY());
+			myTimer = 0;
+		}
+		myTimer = myTimer + Gdx.graphics.getDeltaTime();
 		moving = 0;
+		checkerInt++;
 	}
 
 
